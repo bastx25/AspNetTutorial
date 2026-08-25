eda733e9e72ec6a5c9629a45e281a72b7495b6b9
sroeblreiter <sebastian.roeblreiter@pa-innovations.com>
2026-08-25 16:31:12 +0200

    add DataAnnotations

--- api/Dtos/Comment/CreateCommentDto.cs
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -1,8 +1,16 @@
-﻿namespace api.Dtos.Comment
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos.Comment
 {
     public class CreateCommentDto
     {
+        [Required]
+        [MinLength(5,ErrorMessage = "Title must be 5 Characters long")]
+        [MaxLength(280, ErrorMessage = "Title can nt be longer than 280 characters")]
         public string Title { get; set; } = string.Empty;
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be 5 Characters long")]
+        [MaxLength(280, ErrorMessage = "Content can nt be longer than 280 characters")]
         public string Content { get; set; } = string.Empty;
     }
 }
