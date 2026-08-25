9c6edcb806e370ff03211b6e616f78a0bb0fcb5e
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-08-25 13:35:26 +0200

    add CommentDto, Controller and Mapper

---
+++ api/Controllers/CommentControler.cs
@@ -0,0 +1,38 @@
+﻿using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentControler : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+
+        public CommentControler(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepo.GetAllAsync();
+
+            var commentDto = comments.Select(s => s.ToCommentDto());
+
+            return Ok(commentDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var comment = await _commentRepo.GetByIdAsync(id);
+
+            if (comment == null) return NotFound();
+
+            return Ok(comment.ToCommentDto());
+        }
+    }
+}
---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,21 @@
+﻿using api.Dtos.Comment;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+
+        public static CommentDto ToCommentDto(this Comment commentModel)
+        {
+            return new CommentDto
+            {
+                Id = commentModel.Id,
+                Content = commentModel.Content,
+                CreatedOn = commentModel.CreatedOn,
+                StockId = commentModel.StockId,
+                Title = commentModel.Title,
+            };
+        }
+    }
+}
