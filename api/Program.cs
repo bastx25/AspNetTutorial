cd2e4e2f371a8351dbc53b3b9ba18e1d7f20df5e
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-08-25 18:44:14 +0200

    add TokenService

--- api/Program.cs
+++ api/Program.cs
@@ -3,6 +3,7 @@ using api.Data;
 using api.Interfaces;
 using api.Models;
 using api.Repository;
+using api.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -64,6 +65,7 @@ namespace api
 
             builder.Services.AddScoped<IStockRepository, StockRepository>();
             builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+            builder.Services.AddScoped<ITokenService, TokenService>();
 
             var app = builder.Build();
 
