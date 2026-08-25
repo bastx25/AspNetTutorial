0700c8f430002c4509c69eca0d715075c3fcff12
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-08-25 17:09:22 +0200

    add Nuget Packages for autehntication

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -62,8 +62,6 @@ namespace api.Repository
 
             var skipNumber = (query.PageNumber - 1) * query.PageSize;
 
-
-
             return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
