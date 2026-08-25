ef4b9388b635193b6a21e282d3fb2ffa41924279
sroeblreiter <sebastain.roeblreiter@pa-innovations.com>
2026-08-25 18:51:44 +0200

    Add AccountController

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,12 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
 
-        public AccountController(UserManager<AppUser> userManager)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService  = tokenService;
         }
 
         [HttpPost("register")]
@@ -37,7 +40,13 @@ namespace api.Controllers
 
                     if (roleResult.Succeeded)
                     {
-                        return Ok("User created");
+                        return Ok(
+                            new NewUserDto
+                            {
+                                Username = appUser.UserName,
+                                Email = appUser.Email,
+                                Token = _tokenService.CreateToken(appUser)
+                            });
                     }
                     else
                     {
---
+++ api/Dtos/Account/NewUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Dtos.Account
+{
+    public class NewUserDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+
+    }
+}
